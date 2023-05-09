5e0e15fdb4d53ac3af8145a781bc8342773c54c7
祝小磊 <12610847+snowlie@user.noreply.gitee.com>
2023-05-09 13:34:29 +0000

    修改之前的一些命令

--- exp1.sh
+++ exp1.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
-echo echo $0>>~/.bash_history
-echo man man>>~/.bash_history
-echo who>>~/.bash_history
-echo who am i>>~/.bash_history
-echo date>>~/.bash_history
-echo /vi>>~/.viminfo
-echo ?vi>>~/.viminfo
-cp /tmp/v*[1-3] ~/exp1
-mv ~/exp1/v1 ~/exp1/vi_history_v1
-mv ~/exp1/v2 ~/exp1/vi_history_v2
-mv ~/exp1/v3 ~/exp1/vi_history_v3
-echo 5dd>>~/.viminfo
-echo U>>~/.viminfo
-echo 10x>>~/.viminfo
-echo U>>~/.viminfo
-echo ZZ>>~/.viminfo
-echo ls />>~/.viminfo
-echo mkdir /abc>>~/.bash_history
-echo rmdir /usr>>~/.bash_history
-echo ls>>~/.bash_history
-echo pwd>>~/.bash_history
-echo cd ..>>~/.bash_history
-echo pwd>>~/.bash_history
-echo cd ~>>~/.bash_history
-echo pwd>>~/.bash_history
-echo mkdir ~/exp1/xyz>>~/.bash_history
-echo vi xyz>>~/.bash_history
-echo cat >xyz>>~/.bash_history
-echo ls -l>>~/.bash_history
-echo ls -C>>~/.bash_history
-echo ls -a>>~/.bash_history
-echo rm -rf ~/exp1/xyz>>~/.bash_history
+echo "echo $0">>~/.bash_history
+echo "man man">>~/.bash_history
+echo "who">>~/.bash_history
+echo "who am i">>~/.bash_history
+echo "date">>~/.bash_history
+echo "/vi">>~/.viminfo
+echo "?vi">>~/.viminfo
+echo "The vi history
+The vi editor was developed at the University of california, berkeley
+as part of the berkeley unix system.">>~/exp1/vi_history_1
+echo "The vi history
+The vi editor was developed at the University of california
+berkeley as part of the berkeley unix system.
+At the beginning the vi editor was part of another editor
+The vi part of the ex editor was often used and became very.
+This popularity forced the developers to come up with a separate 
+vi editor.
+now the vi editor is independent of the ex editor and is available on 
+most of the UNIX operating system.
+The vi editor is a good editor for every editing jobs.">>~/exp1/vi_history_2
+echo "The vi history
+The vi editor was developed at the University of California
+Berkeley as part of the Berkeley UNIX system.
+At the beginning the vi (visual) editor was part of the ex editor 
+and you had to be in the ex editor to use the vi editor.
+The vi part of the ex editor was often used and became 
+very popular. This popularity forced the creators to come up 
+with a separate vi editor.
+Now the vi editor is independent of the ex editor and is available 
+on most of the UNIX operating systems.
+The vi editor is a good, efficient editor for every editing jobs 
+although it could be more user friendly.">>~/exp1/vi_history_3
+echo "5dd">>~/.viminfo
+echo "U">>~/.viminfo
+echo "10x">>~/.viminfo
+echo "U">>~/.viminfo
+echo "ZZ">>~/.viminfo
+echo "ls /">>~/.viminfo
+echo "mkdir /abc">>~/.bash_history
+echo "rmdir /usr">>~/.bash_history
+echo "ls">>~/.bash_history
+echo "pwd">>~/.bash_history
+echo "cd ..">>~/.bash_history
+echo "pwd">>~/.bash_history
+echo "cd ~">>~/.bash_history
+echo "pwd">>~/.bash_history
+echo "mkdir ~/exp1/xyz">>~/.bash_history
+echo "vi xyz">>~/.bash_history
+echo "cat >xyz">>~/.bash_history
+echo "ls -l">>~/.bash_history
+echo "ls -C">>~/.bash_history
+echo "ls -a">>~/.bash_history
+echo "rm -rf ~/exp1/xyz">>~/.bash_history
